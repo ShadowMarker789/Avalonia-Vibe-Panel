8ea0a82f3507d56b42d8f9baab620d2f149b9e7b
michael.david.howard@outlook.com <michael.david.howard@outlook.com>
2025-02-22 08:42:23 +0800

    Trying to get this working

--- VibePanel.BrowserDemo/Program.cs
+++ VibePanel.BrowserDemo/Program.cs
@@ -2,13 +2,27 @@
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Browser;
+using Avalonia.Controls.ApplicationLifetimes;
 using VibePanel;
 
 internal sealed partial class Program
 {
-    private static Task Main(string[] args) => BuildAvaloniaApp()
+    private static Task Main(string[] args)
+    {
+        App.OnInit += App_OnInit;
+
+        return BuildAvaloniaApp()
             .WithInterFont()
             .StartBrowserAppAsync("out");
+    }
+
+    private static void App_OnInit(object? sender, Avalonia.Controls.ApplicationLifetimes.IApplicationLifetime? e)
+    {
+        if (e is ISingleViewApplicationLifetime lifetime)
+        {
+            lifetime.MainView = new MainView();
+        }
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>();
