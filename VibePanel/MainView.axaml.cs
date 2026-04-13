ea9b8677bc8e4dd78b833fb9e62d6e44bb0ecb2b
michael.david.howard@outlook.com <michael.david.howard@outlook.com>
2026-04-13 12:47:25 +0800

    Improved the demo and made it more interactive!

--- VibePanel/MainView.axaml.cs
+++ VibePanel/MainView.axaml.cs
@@ -1,57 +1,177 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Data;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace VibePanel;
 
 public partial class MainView : UserControl
 {
-    private CancellationTokenSource cts = new();
-
     public MainView()
     {
         InitializeComponent();
+
+        UpdateTextBlocks();
     }
 
-    protected override async void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    private async void LeftButton_Click(object? sender, RoutedEventArgs args)
     {
         try
         {
-            if (!App.IsBrowser)
+            DisableAllButtons();
+
+            double velocity = 0.0;
+
+            while (VibePanel.GetX(MainRectangle) > -1.0)
             {
-                await AnimateAsync();
+                velocity += 0.001;
+                VibePanel.SetX(MainRectangle, VibePanel.GetX(MainRectangle) - velocity);
+                UpdateTextBlocks();
+                await Task.Delay(7);
             }
-            else
-            {
-                await AnimateAsync();
-            }
-            base.OnAttachedToVisualTree(e);
+
+            VibePanel.SetX(MainRectangle, -1.0);
+
+            EnableAllButtons();
+            UpdateTextBlocks();
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            // we messed up and I'm sorry
+            Console.Error.WriteLine(ex.ToString());
+            EnableAllButtons();
         }
     }
 
-    private async Task AnimateAsync()
+    private async void RightButton_Click(object? sender, RoutedEventArgs args)
     {
-        double x = 0, y = 0;
-
-        System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
-
-        while (!cts.IsCancellationRequested)
+        try
         {
-            x = Math.Cos(sw.Elapsed.TotalSeconds);
-            y = Math.Sin(sw.Elapsed.TotalSeconds * 1.1);
+            DisableAllButtons();
 
-            VibePanel.SetX(MainEllipse, x);
-            VibePanel.SetY(MainEllipse, y);
+            double velocity = 0.0;
 
-            XTextBlock.Text = $"{x:f3}";
-            YTextBlock.Text = $"{y:f3}";
+            while (VibePanel.GetX(MainRectangle) < 1.0)
+            {
+                velocity += 0.001;
+                VibePanel.SetX(MainRectangle, VibePanel.GetX(MainRectangle) + velocity);
+                UpdateTextBlocks();
+                await Task.Delay(7);
+            }
 
-            await Task.Delay(4);
+            VibePanel.SetX(MainRectangle, 1.0);
+
+            EnableAllButtons();
+            UpdateTextBlocks();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.ToString());
+            EnableAllButtons();
         }
     }
+
+    private async void UpButton_Click(object? sender, RoutedEventArgs args)
+    {
+        try
+        {
+            DisableAllButtons();
+
+            double velocity = 0.0;
+
+            while (VibePanel.GetY(MainRectangle) > -1.0)
+            {
+                velocity += 0.001;
+                VibePanel.SetY(MainRectangle, VibePanel.GetY(MainRectangle) - velocity);
+                UpdateTextBlocks();
+                await Task.Delay(7);
+            }
+
+            VibePanel.SetY(MainRectangle, -1.0);
+
+            EnableAllButtons();
+            UpdateTextBlocks();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.ToString());
+            EnableAllButtons();
+        }
+    }
+
+    private async void DownButton_Click(object? sender, RoutedEventArgs args)
+    {
+        try
+        {
+            DisableAllButtons();
+
+            double velocity = 0.0;
+
+            while (VibePanel.GetY(MainRectangle) < 1.0)
+            {
+                velocity += 0.001;
+                VibePanel.SetY(MainRectangle, VibePanel.GetY(MainRectangle) + velocity);
+                UpdateTextBlocks();
+                await Task.Delay(7);
+            }
+
+            VibePanel.SetY(MainRectangle, 1.0);
+
+            EnableAllButtons();
+            UpdateTextBlocks();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.ToString());
+            EnableAllButtons();
+        }
+    }
+
+    private void UpdateTextBlocks()
+    {
+        // too lazy to bind 
+
+        var x = VibePanel.GetX(MainRectangle);
+        var y = VibePanel.GetY(MainRectangle);
+
+        XTextBlock.Text = $"{x:f3}";
+        YTextBlock.Text = $"{y:f3}";
+    }
+
+    private void DisableAllButtons()
+    {
+        LeftButton.IsEnabled = false;
+        RightButton.IsEnabled = false;
+        UpButton.IsEnabled = false;
+        DownButton.IsEnabled = false;
+    }
+
+    private void EnableAllButtons()
+    {
+        LeftButton.IsEnabled = true;
+        RightButton.IsEnabled = true;
+        UpButton.IsEnabled = true;
+        DownButton.IsEnabled = true;
+    }
+
+    //private async Task AnimateAsync()
+    //{
+    //    double x = 0, y = 0;
+
+    //    System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+
+    //    while (!cts.IsCancellationRequested)
+    //    {
+    //        x = Math.Cos(sw.Elapsed.TotalSeconds);
+    //        y = Math.Sin(sw.Elapsed.TotalSeconds * 1.1);
+
+    //        VibePanel.SetX(MainEllipse, x);
+    //        VibePanel.SetY(MainEllipse, y);
+
+    //        XTextBlock.Text = $"{x:f3}";
+    //        YTextBlock.Text = $"{y:f3}";
+
+    //        await Task.Delay(4);
+    //    }
+    //}
 }
